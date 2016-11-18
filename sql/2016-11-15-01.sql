123b7305211f0d11873213dfd7bfcf716b50b725
Dmitry Gavriloff <info@imega.ru>
2016-11-18 16:15:27 +0400

    issue #149 отключил миграцию (#152)

--- sql/2016-11-15-01.sql
+++ sql/2016-11-15-01.sql
@@ -1,7 +1,7 @@
   ALTER TABLE `kv_user`
 CHANGE COLUMN `userId`      `user_id`      CHAR(36),
-CHANGE COLUMN `accessToken` `access_token` VARCHAR(255);
-CHANGE COLUMN `groupId`     `group_id`     CHAR(36);
+CHANGE COLUMN `accessToken` `access_token` VARCHAR(255),
+CHANGE COLUMN `groupId`     `group_id`     CHAR(36),
 CHANGE COLUMN `questId`     `quest_id`     CHAR(36) DEFAULT NULL,
 CHANGE COLUMN `pointId`     `point_id`     CHAR(36) DEFAULT NULL,
 CHANGE COLUMN `logQuest`    `log_quest`    BLOB,
