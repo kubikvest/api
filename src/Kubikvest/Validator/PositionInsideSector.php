737a9fa28b447923d64f1b72e071c59e85ca99a2
Dmitry Gavriloff <info@imega.ru>
2017-01-14 14:17:27 +0400

    issue #212 Ддобавил валидатор входа позиции в сектор (#213)

---
+++ src/Kubikvest/Validator/PositionInsideSector.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (C) 2017. iMega ltd Dmitry Gavriloff (email: info@imega.ru),
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Kubikvest\Validator;
+
+use Kubikvest\Model\Geo\Sector;
+use Kubikvest\Resource\Position\Model\Position;
+
+class PositionInsideSector
+{
+    /**
+     * @param Position $position
+     * @param Sector   $sector
+     *
+     * @return bool
+     */
+    public function validate(Position $position, Sector $sector)
+    {
+        return $sector->getLatitudeRange()->getMin() <= $position->getCoordinate()->getLatitude() &&
+            $sector->getLatitudeRange()->getMax() >= $position->getCoordinate()->getLatitude() &&
+            $sector->getLongitudeRange()->getMin() <= $position->getCoordinate()->getLongitude() &&
+            $sector->getLongitudeRange()->getMax() >= $position->getCoordinate()->getLongitude();
+    }
+}
