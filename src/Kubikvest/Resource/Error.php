c4fed6d6f0392dd2378d10ec1a34fbcca00dd383
Dmitry <irvis@imega.ru>
2017-04-08 20:45:39 +0300

    add err

---
+++ src/Kubikvest/Resource/Error.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright (C) 2017. iMega ltd Dmitry Gavriloff (email: info@imega.ru),
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Kubikvest\Resource;
+
+class Error
+{
+    private $status;
+    private $msg;
+
+    /**
+     * Error constructor.
+     *
+     * @param bool   $status
+     * @param string $msg
+     */
+    public function __construct($status, $msg = "")
+    {
+        $this->status = $status;
+        $this->msg    = $msg;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param bool $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
+    /**
+     * @param string $msg
+     */
+    public function setMsg($msg)
+    {
+        $this->msg = $msg;
+    }
+}
